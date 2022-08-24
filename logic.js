928118e1e01babae6f30c5452b19a67aae6027f3
Nxrio <abby199711@gmail.com>
2022-08-23 23:24:11 -0600

    Logic for Rock Paper Scissors created in a function that plays 1 round.

--- logic.js
+++ logic.js
@@ -5,7 +5,7 @@ function getComputerChoice()
     let random = Math.floor(Math.random() * 10);
     if(random <= 3 && random >=0)
         return "Rock";
-    else if(random <= 4 && random >=3)
+    else if(random <= 6 && random >=4)
         return "Paper";
     else    
         return "Sissors";
